bfb7b144964a6730fbf21845e8f22761f15cc1de
Marwwin <markus.kankkonen@protonmail.com>
2023-01-09 22:19:55 +0200

    histogram

--- src/lib/Histogram/Histogram.svelte
+++ src/lib/Histogram/Histogram.svelte
@@ -9,7 +9,7 @@
         CategoryScale,
         LinearScale,
     } from 'chart.js';
-    import { data } from './data.js';
+    // import { data } from './data.js';
 
     Chart.register(
         Title,
@@ -32,7 +32,69 @@
                 },
             },
         },
-    }
+    };
+    let data = undefined;
+
+    // @ts-ignore
+    const img = new Image(300, 150);
+    img.onload = () => {
+        console.log('load');
+        const canvas = document.getElementById('canvas');
+        // @ts-ignore
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+        const colors = {
+            red: Array(256).fill(0),
+            green: Array(256).fill(0),
+            blue: Array(256).fill(0),
+            luma: Array(256).fill(0),
+        };
+        const imgData = ctx.getImageData(0, 0, img.width, img.height).data;
+        for (let i = 0; i < imgData.length; i += 4) {
+            colors.red[imgData[i]] += 1;
+            colors.green[imgData[i + 1]] += 1;
+            colors.blue[imgData[i + 2]] += 1;
+
+            const gray = Math.round(
+                0.299 * imgData[i] +
+                    0.587 * imgData[i + 1] +
+                    0.114 * imgData[i + 2],
+            );
+            colors.luma[gray] += 1;
+        }
+        console.log(imgData);
+        data = {
+            type: 'bar',
+            labels: Array(256)
+                .fill(null)
+                .map((_, i) => i),
+            datasets: [
+                {
+                    label: 'red',
+                    data: colors.red,
+                    backgroundColor: ['rgba(255, 0,0,0.3)'],
+                },
+                {
+                    label: 'green',
+                    data: colors.green,
+                    backgroundColor: ['rgba(0, 255,0,0.3)'],
+                },
+                {
+                    label: 'blue',
+                    data: colors.blue,
+                    backgroundColor: ['rgba(0, 0,255,0.3)'],
+                },
+            ],
+        };
+    };
+    img.src = 'test.jpeg';
+
+    const normalize = (val, max, min) => (val - min) / (max - min);
 </script>
 
-<Bar width={1000} height={1000} {data} {options} />
+<div>
+    <canvas id="canvas" />
+    {#if data != undefined}
+        <Bar width={800} height={500} {data} {options} />
+    {/if}
+</div>
