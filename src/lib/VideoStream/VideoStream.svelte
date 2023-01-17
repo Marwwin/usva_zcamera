f9e19fad0e394cce72ef26f6633f8926aca7dd4c
Marwwin <markus.kankkonen@protonmail.com>
2023-01-17 22:00:22 +0200

    trying to get stream drawn on canvas

--- src/lib/VideoStream/VideoStream.svelte
+++ src/lib/VideoStream/VideoStream.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
     import { camera } from '../E2Camera';
+
+    let canvas;
+
+    function draw() {
+        const img = document.getElementById('img');
+        d();
+        function d() {
+            const ctx = canvas.getContext('2d');
+            console.log('draw');
+            ctx.drawImage(img, 0, 0);
+            requestAnimationFrame(d);
+        }
+    }
 
     function onClick(e) {
         const y = e.clientY - e.target.offsetTop;
         const x = e.clientX - e.target.offsetLeft;
+
         const width = e.target.width;
         const height = e.target.height;
         console.log(normalize(x, width), normalize(y, height));
         camera.setROI(normalize(x, width), normalize(y, height));
     }
 
-    function normalize(n:number, max:number) {
+    function normalize(n: number, max: number) {
         return (n / max) * 1000;
     }
 </script>
@@ -18,5 +33,16 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-missing-attribute -->
 <img
-    on:click={(e) => onClick(e)}
+    on:load={() => draw()}
+    id="img"
     src={`http://${import.meta.env.VITE_CAMERA}/mjpeg_stream`} />
+
+<canvas
+    width="1600px;"
+    height="1200px;"
+    on:click={(e) => onClick(e)}
+    bind:this={canvas}
+    id="stream" />
+
+<style>
+</style>
